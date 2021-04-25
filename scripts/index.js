2a4be3df6f053368d3061b04f2bfe02cc5840ced
Kashankov Vladimi <vkashankov@yandex.ru>
2021-04-25 13:27:45 +0300

    hotfix/bem_js

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,24 @@
-let openPopupButton = document.querySelector('.profile__editButton'); //Выбираем кнопку редактирования профиля
+let openPopupButton = document.querySelector('.profile__editbutton'); //Выбираем кнопку редактирования профиля
 let popup = document.querySelector('.popup'); // Выбираем сам попап для использования этой константы в функции
 let closePopupButton = document.querySelector('.popup__close'); //Выбираем крест для закрытия попапа
 let popupButtonSave = document.querySelector('.popup__save');//Выбираем кнопку Сохранить
 
 // Функция которая добавляет и удалет класс
 function openClose() {
-  popup.classList.toggle('popupOpen');
+  popup.classList.toggle('popup_open');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileAbout.textContent;
 }
 // Открываем попап
 openPopupButton.addEventListener('click', openClose)
 closePopupButton.addEventListener('click', openClose)
-popupButtonSave.addEventListener('click', openClose)
+
 
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__body');// Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__name');// Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.popup__about');// Воспользуйтесь инструментом .querySelector()
+let nameInput = document.querySelector('.popup__input_name');// Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('.popup__input_about');// Воспользуйтесь инструментом .querySelector()
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 // Обработчик «отправки» формы, хотя пока
@@ -33,8 +35,10 @@ function formSubmitHandler (evt) {
     // Вставьте новые значения с помощью textContent
     profileName.textContent = nameInput.value;
     profileAbout.textContent = jobInput.value;
+    openClose ();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+popupButtonSave.addEventListener('click', formSubmitHandler);
