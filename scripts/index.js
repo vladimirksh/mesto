fb669f295980986cd48d473142db76db2c825fdb
Kashankov Vladimi <vkashankov@yandex.ru>
2021-06-16 22:51:42 +0300

    Lust change

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,9 @@
   const template = document.querySelector('#template').content;//выбраем заготовку по id и запрашиваем внутренности
   const cardsContainer = document.querySelector('.cards-container');//место куда копируем новые карточки
   const element = template.querySelector('.element').cloneNode(true);
-
   const popupImage = document.querySelector('.popup__image');
   const popupText = document.querySelector('.popup__text');
 
-  
-
-
-
-  
 //функция создания одной карочки с кнопкоми и попапом
   function createCard (name, link) {
     const element = template.querySelector('.element').cloneNode(true);
@@ -31,7 +25,7 @@
     popupImage.setAttribute('src', element.querySelector('.element__image').src);//ссылка для картинки
     popupImage.setAttribute('alt',element.querySelector('.element__title').textContent);//alt для картинки
     popupText.textContent = element.querySelector('.element__title').textContent;//подпись снизу для картинки
-    openCloseZoom();
+    openPopup(popupZoom);
     });
     return element;
   }
@@ -41,37 +35,20 @@ initialCards.forEach (function(item){
   cardsContainer.append(createCard(item.name,item.link));
 });
 
-
-
+//Закрываем увеличиельный попап
 const popupZoom = document.querySelector('.popup_zoom-image');
 const popupCloseZoom = document.querySelector('.popup__close-zoom');
-function openCloseZoom() {
-  popupZoom.classList.toggle('popup_open');
-}
-
-popupCloseZoom.addEventListener('click', openCloseZoom)
-
-
-
-
-
-
-
-
-
+popupCloseZoom.addEventListener('click', () => closePopup(popupZoom));
 
 // Popup для новых постов
 const openPopupAddButton = document.querySelector('.profile__addbutton'); //Выбираем кнопку вызывающую попап
 const popupAddCard = document.querySelector('.popup_add-card'); // Выбираем сам попап для использования этой константы в функции
 const closePopupAddButton = document.querySelector('.popup__close_add-card'); //Выбираем крест для закрытия попапа
 const savePopupAddButton = document.querySelector('.popup__save_add-card');//Выбираем кнопку Создать
-// Функция которая добавляет и удалет класс
-function openCloseAddCard() {
-  popupAddCard.classList.toggle('popup_open');
-}
+
 // Открываем попап для добавления нового поста
-openPopupAddButton.addEventListener('click', openCloseAddCard)
-closePopupAddButton.addEventListener('click', openCloseAddCard)
+openPopupAddButton.addEventListener('click', () => openPopup(popupAddCard));
+closePopupAddButton.addEventListener('click', () => closePopup(popupAddCard));
 // Находим форму в DOM для добавления нового поста
 const formElementAddcard = document.querySelector('.popup__body_add-card');
 
@@ -95,40 +72,18 @@ function formSubmitNewCard (evt) {
     //попап для картинок
     element.querySelector('.element__image').addEventListener('click', function (evt) {
       popupImage.setAttribute('src', element.querySelector('.element__image').src);
+      popupImage.setAttribute('alt', element.querySelector('.element__title').textContent);
       popupText.textContent = element.querySelector('.element__title').textContent;
-      evt.target.classList.toggle(openCloseZoom());
+      openPopup(popupZoom);
   });
 
   cardsContainer.prepend(element);
-  openCloseAddCard();
+  closePopup(popupAddCard);
   formElementAddcard.reset();//сбрасываем содержимое при повторном открытии попапа
 }
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElementAddcard.addEventListener('submit', formSubmitNewCard);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 const openPopupButton = document.querySelector('.profile__editbutton'); //Выбираем кнопку редактирования профиля
@@ -172,6 +127,5 @@ function formSubmitHandler (evt) {
     profileAbout.textContent = jobInput.value;
     closePopup(popupChangeName);
 }
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+
 formElement.addEventListener('submit', formSubmitHandler);
